4e94da21812d512ba012aa3fc1d3ae02e9a95d3c
Keerthy <j-keerthy@ti.com>
2019-02-11 11:38:19 +0530

    include: configs: j721e_evm: Add env variables for main_r5fss0_core0
    Add env variables for main_r5fss0_core0 firmware loadaddr and name.
    
    Signed-off-by: Keerthy <j-keerthy@ti.com>
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- include/configs/j721e_evm.h
+++ include/configs/j721e_evm.h
@@ -87,6 +87,8 @@
 	"mmcdev=1\0"							\
 	"bootpart=1:2\0"						\
 	"bootdir=/boot\0"						\
+	"mainr5f0_0loadaddr=88000000\0"					\
+	"mainr5f0_0fwname=/lib/firmware/j7-main-r5f0_0-fw\0"		\
 	"rd_spec=-\0"							\
 	"init_mmc=run args_all args_mmc\0"				\
 	"get_fdt_mmc=load mmc ${bootpart} ${fdtaddr} ${bootdir}/${fdtfile}\0" \
