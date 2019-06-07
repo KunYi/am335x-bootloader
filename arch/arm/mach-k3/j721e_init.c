c26f06b51d26894ddd00dfbb48893e0c2e10f0c9
Keerthy <j-keerthy@ti.com>
2019-06-07 21:14:42 +0530

    arm: mach-k3: j721e_init: Initialize avs class 0
    Initialize avs class 0
    
    Signed-off-by: Keerthy <j-keerthy@ti.com>

--- arch/arm/mach-k3/j721e_init.c
+++ arch/arm/mach-k3/j721e_init.c
@@ -126,6 +126,12 @@ void board_init_f(ulong dummy)
 	preloader_console_init();
 #endif
 
+#if defined(CONFIG_CPU_V7R) && defined(CONFIG_K3_AVS0)
+	ret = uclass_get_device(UCLASS_AVS, 0, &dev);
+	if (ret)
+		printf("AVS init failed: %d\n", ret);
+#endif
+
 #if defined(CONFIG_K3_J721E_DDRSS)
 	ret = uclass_get_device(UCLASS_RAM, 0, &dev);
 	if (ret)
