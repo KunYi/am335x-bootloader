aed252f590bccc986fc64908726b32e6e3e07a88
Jean-Jacques Hiblot <jjhiblot@ti.com>
2019-09-19 12:55:42 +0200

    regmap: Add devm_regmap_init()
    Most of new linux drivers are using managed-API to allocate resources. To
    ease porting drivers from linux to u-boot, introduce devm_regmap_init() as
    a managed API to get a regmap from the device tree.
    
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

--- drivers/core/regmap.c
+++ drivers/core/regmap.c
@@ -219,6 +219,32 @@ int regmap_init_mem(ofnode node, struct regmap **mapp)
 
 	return 0;
 }
+
+static void devm_reset_release(struct udevice *dev, void *res)
+{
+	regmap_uninit(res);
+}
+
+struct regmap *devm_regmap_init(struct udevice *dev,
+				const struct regmap_bus *bus,
+				void *bus_context,
+				const struct regmap_config *config)
+{
+	int rc;
+	struct regmap **mapp;
+
+	mapp = devres_alloc(devm_reset_release, sizeof(struct regmap *),
+			    __GFP_ZERO);
+	if (unlikely(!mapp))
+		return ERR_PTR(-ENOMEM);
+
+	rc = regmap_init_mem(dev_ofnode(dev), mapp);
+	if (rc)
+		return ERR_PTR(rc);
+
+	devres_add(dev, mapp);
+	return *mapp;
+}
 #endif
 
 void *regmap_get_range(struct regmap *map, unsigned int range_num)
