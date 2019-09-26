KunYi/am335x-bootloader

commit 1d9fb73cad298d36fd3138e6b933a0c6399457ad

arm: mach-k3: sysfw-loader: Call PM config done callback asap
Call config_pm_done_callback() as soon as all clocks and PM aspects are
setup. This is required because SYSFW may reconfigure clocks after PM
configs are applied leading to boot media being inaccessible as is the
case with HyperBus (bus clock is at wrong frequency), resulting in
failures when loading the RM and Security configs.

Since UART may not be configured at this stage, move printing of SYSFW
version info message to separate function so that it can be invoked by
individual board files after console initialization. Make corresponding
changes to board_init_f() of AM6 and J721E.

Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
Signed-off-by: Andreas Dannenberg <dannenberg@ti.com>

arch/arm/mach-k3/sysfw-loader.c
@@ -158,6 +158,15 @@ static void k3_sysfw_configure_using_fit(void *fit,
 		hang();
 	}
 
+	/*
+	 * Now that all clocks and PM aspects are setup, invoke a user-
+	 * provided callback function. Usually this callback would be used
+	 * to setup or reconfigure any clocks that may need updated values
+	 * as SYSFW PM init can changes the defaults.
+	 */
+	if (config_pm_done_callback)
+		config_pm_done_callback();
+
 	/* Apply resource management (RM) configuration to SYSFW */
 	ret = board_ops->board_config_rm(ti_sci,
 					 (u64)(u32)cfg_fragment_addr,
@@ -185,14 +194,6 @@ static void k3_sysfw_configure_using_fit(void *fit,
 		       ret);
 		hang();
 	}
-
-	/*
-	 * Now that all clocks and PM aspects are setup, invoke a user-
-	 * provided callback function. Usually this callback would be used
-	 * to setup or re-configure the U-Boot console UART.
-	 */
-	if (config_pm_done_callback)
-		config_pm_done_callback();
 }
 
 #if CONFIG_IS_ENABLED(SPI_LOAD)
@@ -320,8 +321,16 @@ void k3_sysfw_loader(void (*config_pm_done_callback)(void))
 
 	/* Parse and apply the different SYSFW configuration fragments */
 	k3_sysfw_configure_using_fit(addr, ti_sci, config_pm_done_callback);
+}
 
-	/* Output System Firmware version info */
+/* Output System Firmware version info */
+void k3_sysfw_loader_print_ver(void)
+{
+	struct ti_sci_handle *ti_sci = get_ti_sci_handle();
+
+	if (!ti_sci)
+		return;
+
 	printf("SYSFW ABI: %d.%d (firmware rev 0x%04x '%.*s')\n",
 	       ti_sci->version.abi_major, ti_sci->version.abi_minor,
 	       ti_sci->version.firmware_revision,

arch/arm/mach-k3/am6_init.c
@@ -86,7 +86,6 @@ static void reinit_mmc_device(int dev)
 
 void k3_init_post_pm_cb(void)
 {
-	preloader_console_init();
 	if (spl_boot_device() == BOOT_DEVICE_MMC1)
 		reinit_mmc_device(0);
 	else if (spl_boot_device() == BOOT_DEVICE_MMC2)
@@ -147,15 +146,18 @@ void board_init_f(ulong dummy)
 	}
 
 	/*
-	 * Load, start up, and configure system controller firmware. Provide
-	 * the U-Boot console init function to the SYSFW post-PM configuration
-	 * callback hook, effectively switching on (or over) the console
-	 * output.
+	 * Load, start up, and configure system controller firmware while
+	 * also populating the SYSFW post-PM configuration callback hook.
 	 */
 	k3_sysfw_loader(k3_init_post_pm_cb);
-#else
+#endif
+
 	/* Prepare console output */
 	preloader_console_init();
+
+#ifdef CONFIG_K3_LOAD_SYSFW
+	/* Output System Firmware version info */
+	k3_sysfw_loader_print_ver();
 #endif
 
 	/* Perform EEPROM-based board detection */

arch/arm/mach-k3/include/mach/sysfw-loader.h
@@ -8,5 +8,6 @@
 #define _SYSFW_LOADER_H_
 
 void k3_sysfw_loader(void (*config_pm_done_callback)(void));
+void k3_sysfw_loader_print_ver(void);
 
 #endif

arch/arm/mach-k3/j721e_init.c
@@ -128,16 +128,16 @@ void board_init_f(ulong dummy)
 	if (!ret)
 		pinctrl_select_state(dev, "default");
 
-	/*
-	 * Load, start up, and configure system controller firmware. Provide
-	 * the U-Boot console init function to the SYSFW post-PM configuration
-	 * callback hook, effectively switching on (or over) the console
-	 * output.
-	 */
-	k3_sysfw_loader(preloader_console_init);
-#else
+	/* Load, start up, and configure system controller firmware */
+	k3_sysfw_loader(NULL);
+#endif
+
 	/* Prepare console output */
 	preloader_console_init();
+
+#ifdef CONFIG_K3_LOAD_SYSFW
+	/* Output System Firmware version info */
+	k3_sysfw_loader_print_ver();
 #endif
 
 	/* Perform EEPROM-based board detection */

commit d7a6666972094de2cdcab045e9dcc69ea47b0d1c

arm: mach-k3: sysfw-loader: Fix loading of SYSFW during HyperFlash boot
In case of HyperFlash boot on J721E, SYSFW reconfigures HyperFlash
bus clock as soon as PM configuration is sent. Therefore it is necessary
to reconfigure clocks back to boot frequency, which is currently done in
config_pm_done_callback(). But this callback is called after
extracting RM configuration pointer from FIT image and before applying
it. Since, fit_get_data_by_name() (that is used to extract RM
configuration) itself accesses HyperFlash, its necessary that clocks
are reconfigured before that. Therefore move call to
fit_get_data_by_name() after calling config_pm_done_callback().

With this HyperFlash boot works reliably at 83MHz.

Fixes: 1d9fb73cad29 ("arm: mach-k3: sysfw-loader: Call PM config done callback asap")
Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

arch/arm/mach-k3/sysfw-loader.c
@@ -153,15 +153,6 @@ static void k3_sysfw_configure_using_fit(void *fit,
 		hang();
 	}
 
-	/* Extract resource management (RM) specific configuration from FIT */
-	ret = fit_get_data_by_name(fit, images, SYSFW_CFG_RM,
-				   &cfg_fragment_addr, &cfg_fragment_size);
-	if (ret < 0) {
-		printf("Error accessing %s node in FIT (%d)\n", SYSFW_CFG_RM,
-		       ret);
-		hang();
-	}
-
 	/*
 	 * Now that all clocks and PM aspects are setup, invoke a user-
 	 * provided callback function. Usually this callback would be used
@@ -170,6 +161,15 @@ static void k3_sysfw_configure_using_fit(void *fit,
 	 */
 	if (config_pm_done_callback)
 		config_pm_done_callback();
+
+	/* Extract resource management (RM) specific configuration from FIT */
+	ret = fit_get_data_by_name(fit, images, SYSFW_CFG_RM,
+				   &cfg_fragment_addr, &cfg_fragment_size);
+	if (ret < 0) {
+		printf("Error accessing %s node in FIT (%d)\n", SYSFW_CFG_RM,
+		       ret);
+		hang();
+	}
 
 	/* Apply resource management (RM) configuration to SYSFW */
 	ret = board_ops->board_config_rm(ti_sci,