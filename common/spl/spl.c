532681ef1dff9bcfaff21138cfe56dda3a8754c5
Lokesh Vutla <lokeshvutla@ti.com>
2019-10-01 15:23:08 +0530

    Revert "spl: enable caches on all armv7r platforms"
    This reverts commit eda701f54c8535025943203bb070381ade6b817e.

--- common/spl/spl.c
+++ common/spl/spl.c
@@ -580,8 +580,7 @@ void board_init_r(gd_t *dummy1, ulong dummy2)
 	spl_set_bd();
 
 #if !(defined(CONFIG_SYS_ICACHE_OFF) && defined(CONFIG_SYS_DCACHE_OFF)) && \
-	(defined(CONFIG_CPU_V7A) || defined(CONFIG_ARM64) ||	\
-	 defined(CONFIG_CPU_V7R))
+		(defined(CONFIG_CPU_V7A) || defined(CONFIG_ARM64))
 	dram_init_banksize();
 	reserve_mmu();
 	enable_caches();
